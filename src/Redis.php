2a59682f9a6c2d2ab401c389e20210eeecff2962
elliot <https://Apache-HB@github.com>
2021-07-06 21:28:21 +0100

    add documentation

--- src/Redis.php
+++ src/Redis.php
@@ -116,6 +116,29 @@ class RedisFeatureRequester extends FeatureRequesterBase
 
 class Redis
 {
+    /**
+     * Configures an adapter for reading feature flag data from Redis using persistent connections.
+     *
+     * To use this method, you must have installed the `phpredis` extension. After calling this
+     * method, store its return value in the `feature_requester` property of your client configuration:
+     *
+     *     $fr = LaunchDarkly\Integrations\PHPRedis::featureRequester([ "redis_prefix" => "env1" ]);
+     *     $config = [ "feature_requester" => $fr ];
+     *     $client = new LDClient("sdk_key", $config);
+     *
+     * For more about using LaunchDarkly with databases, see the
+     * [SDK reference guide](https://docs.launchdarkly.com/v2.0/docs/using-a-persistent-feature-store).
+     *
+     * @param array $options  Configuration settings (can also be passed in the main client configuration):
+     *   - `redis_host`: hostname of the Redis server; defaults to `localhost`
+     *   - `redis_port`: port of the Redis server; defaults to 6379
+     *   - `redis_timeout`: connection timeout in seconds; defaults to 5
+     *   - `redis_prefix`: a string to be prepended to all database keys; corresponds to the prefix
+     * setting in ld-relay
+     *   - `phpredis_client`: an already-configured Predis client instance if you wish to reuse one
+     *   - `apc_expiration`: expiration time in seconds for local caching, if `APCu` is installed
+     * @return mixed  an object to be stored in the `feature_requester` configuration property
+     */
     public static function persistentFeatureRequester($options = array())
     {
         return function ($baseUri, $sdkKey, $baseOptions) use ($options) {
@@ -123,6 +146,29 @@ class Redis
         };
     }
 
+    /**
+     * Configures an adapter for reading feature flag data from Redis.
+     *
+     * To use this method, you must have installed the package `predis/predis`. After calling this
+     * method, store its return value in the `feature_requester` property of your client configuration:
+     *
+     *     $fr = LaunchDarkly\Integrations\Redis::featureRequester([ "redis_prefix" => "env1" ]);
+     *     $config = [ "feature_requester" => $fr ];
+     *     $client = new LDClient("sdk_key", $config);
+     *
+     * For more about using LaunchDarkly with databases, see the
+     * [SDK reference guide](https://docs.launchdarkly.com/v2.0/docs/using-a-persistent-feature-store).
+     *
+     * @param array $options  Configuration settings (can also be passed in the main client configuration):
+     *   - `redis_host`: hostname of the Redis server; defaults to `localhost`
+     *   - `redis_port`: port of the Redis server; defaults to 6379
+     *   - `redis_timeout`: connection timeout in seconds; defaults to 5
+     *   - `redis_prefix`: a string to be prepended to all database keys; corresponds to the prefix
+     * setting in ld-relay
+     *   - `predis_client`: an already-configured Predis client instance if you wish to reuse one
+     *   - `apc_expiration`: expiration time in seconds for local caching, if `APCu` is installed
+     * @return mixed  an object to be stored in the `feature_requester` configuration property
+     */
     public static function featureRequester($options = array())
     {
         return function ($baseUri, $sdkKey, $baseOptions) use ($options) {
