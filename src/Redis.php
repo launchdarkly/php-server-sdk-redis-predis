b0637ed669c7a34203a69d7f2f111ae40789d1b3
Apache-HB <apachehaisley@gmail.com>
2021-07-28 16:56:48 +0100

    change docstrings and add types

--- src/Redis.php
+++ src/Redis.php
@@ -1,20 +1,22 @@
 <?php
 namespace LaunchDarkly\Integrations;
 
+/**
+ * Integration with a Redis data store using the `predis` package.
+ */
 class Redis
 {
     /**
      * Configures an adapter for reading feature flag data from Redis.
      *
-     * To use this method, you must have installed the package `predis/predis`. After calling this
-     * method, store its return value in the `feature_requester` property of your client configuration:
+     * After calling this method, store its return value in the `feature_requester` property of your client configuration:
      *
      *     $fr = LaunchDarkly\Integrations\Redis::featureRequester([ "redis_prefix" => "env1" ]);
      *     $config = [ "feature_requester" => $fr ];
      *     $client = new LDClient("sdk_key", $config);
      *
      * For more about using LaunchDarkly with databases, see the
-     * [SDK reference guide](https://docs.launchdarkly.com/v2.0/docs/using-a-persistent-feature-store).
+     * [SDK reference guide](https://docs.launchdarkly.com/sdk/features/storing-data).
      *
      * @param array $options  Configuration settings (can also be passed in the main client configuration):
      *   - `redis_host`: hostname of the Redis server; defaults to `localhost`
@@ -26,9 +28,9 @@ class Redis
      *   - `apc_expiration`: expiration time in seconds for local caching, if `APCu` is installed
      * @return mixed  an object to be stored in the `feature_requester` configuration property
      */
-    public static function featureRequester($options = array())
+    public static function featureRequester(array $options = [])
     {
-        return function ($baseUri, $sdkKey, $baseOptions) use ($options) {
+        return function (string $baseUri, string $sdkKey, array $baseOptions) use ($options) {
             return new Impl\RedisFeatureRequester($baseUri, $sdkKey, array_merge($baseOptions, $options));
         };
     }
--- src/RedisFeatureRequester.php
+++ src/RedisFeatureRequester.php
@@ -7,13 +7,16 @@ use Predis\Client;
 
 class RedisFeatureRequester extends FeatureRequesterBase
 {
+    /** @var ClientInterface */
     var $_connection;
+    /** @var array */
     var $_options;
+    /** @var string */
     var $_prefix;
 
-    public function __constructor($baseUri, $sdkKey, $options)
+    public function __construct(string $baseUri, string $sdkKey, string $options)
     {
-        parent::__constructor($baseUri, $sdkKey, $options);
+        parent::__construct($baseUri, $sdkKey, $options);
 
         $this->$_prefix = $options['redis_prefix'] ?? 'launchdarkly';
 
@@ -30,20 +33,20 @@ class RedisFeatureRequester extends FeatureRequesterBase
         }
     }
     
-    protected function readItemString($namespace, $key)
+    protected function readItemString(string $namespace, string $key): ?string
     {
         $redis = $this->getConnection();
         return $redis->hget("$this->_prefix:$namespace", $key);
     }
 
-    protected function readItemStringList($namespace)
+    protected function readItemStringList(string $namespace): ?array
     {
         $redis = $this->getConnection();
         $raw = $redis->hgetall("$this->_prefix:$namespace");
         return $raw ? array_values($raw) : null;
     }
 
-    protected function getConnection()
+    protected function getConnection(): ClientInterface
     {
         if ($this->$_connection != null) {
             return $this->$_connection;
