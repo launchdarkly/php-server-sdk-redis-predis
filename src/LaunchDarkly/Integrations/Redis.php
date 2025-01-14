869108ba0a011a3f1b89fa9b9a814896ef4f32d9
Matthew Keeler <mkeeler@launchdarkly.com>
2025-01-14 11:12:57 -0500

    feat: FeatureRequester requires configured ClientInterface

--- src/LaunchDarkly/Integrations/Redis.php
+++ src/LaunchDarkly/Integrations/Redis.php
@@ -2,8 +2,8 @@
 
 namespace LaunchDarkly\Integrations;
 
-use LaunchDarkly\Impl\Integrations\RedisFeatureRequester;
 use LaunchDarkly\Impl\Integrations\RedisBigSegmentsStore;
+use LaunchDarkly\Impl\Integrations\RedisFeatureRequester;
 use LaunchDarkly\Subsystems;
 use Predis\ClientInterface;
 use Psr\Log\LoggerInterface;
@@ -20,39 +20,29 @@ class Redis
      *
      * After calling this method, store its return value in the `feature_requester` property of your client configuration:
      *
-     *     $fr = LaunchDarkly\Integrations\Redis::featureRequester([ "redis_prefix" => "env1" ]);
-     *     $config = [ "feature_requester" => $fr ];
+     *     $fr = LaunchDarkly\Integrations\Redis::featureRequester(["prefix" => "env1"]);
+     *     $config = ["feature_requester" => $fr];
      *     $client = new LDClient("sdk_key", $config);
      *
      * For more about using LaunchDarkly with databases, see the
      * [SDK reference guide](https://docs.launchdarkly.com/sdk/features/storing-data).
      *
-     * @param array $options  Configuration settings (can also be passed in the main client configuration):
-     *   - `redis_host`: hostname of the Redis server; defaults to `localhost`
-     *   - `redis_port`: port of the Redis server; defaults to 6379
-     *   - `redis_timeout`: connection timeout in seconds; defaults to 5
-     *   - `redis_prefix`: a string to be prepended to all database keys; corresponds to the prefix
-     * setting in ld-relay
-     *   - `predis_client`: an already-configured Predis client instance if you wish to reuse one; if
-     * specified, this will cause all other options except `redis_prefix` and `apc_expiration`
-     * to be ignored
-     *   - `predis_options`: an array of connection options to be passed directly to the Predis client;
-     * these can include any options supported by `Predis\Client`, and will override any equivalent
-     * top-level options (for instance, `"host"` in `predis_options` takes priority over `"redis_host"`
-     * at the top level)
-     *   - `apc_expiration`: expiration time in seconds for local caching, if `APCu` is installed
-     * @return mixed  an object to be stored in the `feature_requester` configuration property
+     * @param array<string, mixed> $options Configuration settings (can also be passed in the main client configuration):
+     *   - `prefix`: a string to be prepended to all database keys; corresponds
+     *   to the prefix setting in ld-relay
+     * @return callable(string, string, array): Subsystems\FeatureRequester
      */
-    public static function featureRequester(array $options = [])
+    public static function featureRequester(ClientInterface $client, array $options = []): callable
     {
-        return function (string $baseUri, string $sdkKey, array $baseOptions) use ($options) {
-            return new RedisFeatureRequester($baseUri, $sdkKey, array_merge($baseOptions, $options));
+        return function (string $baseUri, string $sdkKey, array $baseOptions) use ($client, $options): Subsystems\FeatureRequester {
+            return new RedisFeatureRequester($client, $baseUri, $sdkKey, array_merge($baseOptions, $options));
         };
     }
 
     /**
      * @param array<string,mixed> $options
-     *   - `prefix`: namespace prefix to add to all hash keys
+     *   - `prefix`: a string to be prepended to all database keys; corresponds
+     *   to the prefix setting in ld-relay
      * @return callable(LoggerInterface, array): Subsystems\BigSegmentsStore
      */
     public static function bigSegmentsStore(ClientInterface $client, array $options = []): callable
